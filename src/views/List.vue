258fc8980a749809df754336bcea951eb1a4ce6e
Allan Dorr <aldorrman@gmail.com>
2019-06-28 16:49:58 +0200

    List actually lists entries.

--- src/views/List.vue
+++ src/views/List.vue
@@ -3,11 +3,15 @@
     <div id="is-time-calendar" class="section is-view">
         <div class="container">
             <div class="hero is-small is-warning">
-                <div class="hero-body"><h2 class="title">Just a test page</h2></div>
+                <div class="hero-body">
+                    <h2 class="title">Just a test page</h2>
+                    <p>(but with real data from user {{currentUserName}})</p>
+                </div>
             </div>
         </div>
         <div class="container">
-            <div class="columns">
+
+<!--             <div class="columns">
                 <div class="column">
                     <table class="table is-striped is-fullwidth">
                         <thead>
@@ -256,8 +260,119 @@
                         </tbody>
                     </table>
                 </div>
-            </div>
+            </div> -->
+
+            <b-table :data="currentTimeEntries" :columns="columns"></b-table>
         </div>
     </div>
     <!-- END CALENDAR VIEW -->
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                // timeEntries: {},
+                columns: [
+                    {
+                        field: 'customer',
+                        label: 'Kunde'
+                    },
+                    {
+                        field: 'area',
+                        label: 'Bereich'
+                    },
+                    {
+                        field: 'job',
+                        label: 'Job'
+                    },
+                    {
+                        field: 'date',
+                        label: 'Datum'
+                    },
+                    {
+                        field: 'time',
+                        label: 'Zeit'
+                    },
+                    {
+                        field: 'note',
+                        label: 'Notiz'
+                    }
+                ],
+            }
+        },
+        computed: {
+            userID() {
+                return this.$store.getters.user
+            },
+            timeEntries() {
+                return this.$store.getters.usertimes
+            },
+            currentUserName() {
+                // get user's name with the id this.$store.getters.user
+                let userID = this.$store.getters.user
+                // let timeEntries = this.timeEntries
+                let currentUserName = this.timeEntries[userID].fullname
+                // console.log(currentUserName)
+                return currentUserName
+            },
+            currentTimeEntries() {
+                let userID = this.userID
+                // console.log(userID)
+                let timeEntries = this.timeEntries
+                // console.log(timeEntries)
+                let userObject = timeEntries[userID]
+                // console.log(userObject)
+                let currentTimeEntriesObject = userObject.timeentries
+                // console.log(currentTimeEntriesObject)
+                let currentTimeEntriesArray = this.json2array(currentTimeEntriesObject)
+                // also iterate through array: change dates to readable format
+                // & change time to hours & mins
+                currentTimeEntriesArray.forEach((v, i) =>
+                    currentTimeEntriesArray[i].date = this.dateToHuman(currentTimeEntriesArray[i].date)
+                );
+                currentTimeEntriesArray.forEach((v, i) =>
+                    currentTimeEntriesArray[i].time = this.secondsToHrsMins(currentTimeEntriesArray[i].time)
+                );
+                return currentTimeEntriesArray
+            }
+        },
+        methods: {
+            loadAllData() {
+                this.$store.dispatch('loadCustomerEntries')
+                this.$store.dispatch('loadTimeEntries')
+                .then(() => {
+                    this.timeEntries = this.$store.state.userTimeEntries
+                    this.userName = this.currentUserName
+                })
+            },
+            json2array(json){
+                var result = [];
+                var keys = Object.keys(json);
+                keys.forEach(function(key){
+                    result.push(json[key]);
+                });
+                return result;
+            },
+            dateToHuman(dateString) {
+                let date = new Date(dateString)
+                let dateHuman
+                let year    = date.getFullYear();
+                let month   = date.getMonth() + 1;
+                let day     = date.getDate();
+                dateHuman = day + '.' + month + '.' + year
+                return dateHuman
+            },
+            secondsToHrsMins(seconds) {
+                let mins = seconds / 60
+                let hrs = Math.floor(mins / 60)
+                mins = mins % 60
+                return hrs + 'hrs ' + mins + 'mins'
+            }
+        },
+        mounted() {
+            this.loadAllData()
+            // this.timeEntries = this.$store.state.userTimeEntries
+        }
+    }
+</script>
