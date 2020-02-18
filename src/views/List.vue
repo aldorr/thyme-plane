b85d4c082c04c292e332302a99af3e02c25c5d38
Ozan Alatur <ozan.alatur@informatik.hu-berlin.de>
2020-02-18 14:26:02 +0100

    Delete Commented Section && change of UserID cause not working for some Accounts

--- src/views/List.vue
+++ src/views/List.vue
@@ -11,257 +11,6 @@
         </div>
         <div class="container">
 
-<!--             <div class="columns">
-                <div class="column">
-                    <table class="table is-striped is-fullwidth">
-                        <thead>
-                            <tr>
-                                <th>Date</th>
-                                <th>Project</th>
-                                <th>Category</th>
-                                <th>Subcategory</th>
-                                <th>Time Entry</th>
-                            </tr>
-                        </thead>
-                        <tfoot>
-                            <tr>
-                                <th>Date</th>
-                                <th>Project</th>
-                                <th>Category</th>
-                                <th>Subcategory</th>
-                                <th>Time Entry</th>
-                            </tr>
-                        </tfoot>
-                        <tbody>
-                            <tr>
-                                <th>01/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th></th>
-                                <td>Project 3</td>
-                                <td>Category a</td>
-                                <td>Subcategory 3fu</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr class="is-weekend">
-                                <th>02/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr class="is-weekend">
-                                <th>03/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>04/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>05/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>06/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>07/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>08/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>09/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>10/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>11/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>12/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>13/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>14/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>15/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>16/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>17/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>18/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>19/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>20/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>21/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>22/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>23/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>24/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>25/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>26/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>27/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>28/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>29/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>30/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                            <tr>
-                                <th>31/06/2019</th>
-                                <td>Project 2</td>
-                                <td>Category 2</td>
-                                <td>Subcategory 2</td>
-                                <td>2h 15m</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div> -->
-
             <b-table
                 :data="currentTimeEntries"
                 :columns="columns"
@@ -313,14 +62,14 @@
         },
         computed: {
             userID() {
-                return this.$store.getters.user
+                return 'ZVhFit5urSeId1YrbJXHMFWKVeq2'
             },
             timeEntries() {
                 return this.$store.getters.usertimes
             },
             currentUserName() {
                 // get user's name with the id this.$store.getters.user
-                let userID = this.$store.getters.user
+                let userID = this.userID
                 // let timeEntries = this.timeEntries
                 let currentUserName = this.timeEntries[userID].fullname
                 // console.log(currentUserName)
@@ -382,7 +131,7 @@
         },
         mounted() {
             this.loadAllData()
-            // this.timeEntries = this.$store.state.userTimeEntries
+            
         }
     }
 </script>
